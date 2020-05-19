f7b4fc5e8102f02d87de9df58492b559a36652a4
olanrewaju Makinde <lanre.developer@gmail.com>
2020-05-19 22:05:13 +0100

    an app wide axios hook class

---
+++ src/axios/constants.js
@@ -0,0 +1,9 @@
+export const MAIN_API = "http://localhost:3002/api/";
+export const MAIL_API = "http://localhost:3002/api/v1/mailer/";
+
+export const API_STATUS = {
+  SERVICE_ERROR: "WEB_SERVICE_REQUEST_ERROR",
+  SERVICE_IDLE: "WEB_SERVICE_REQUEST_IDLE",
+  SERVICE_PENDING: "WEB_SERVICE_REQUEST_PENDING",
+  SERVICE_SUCCESS: "WEB_SERVICE_REQUEST_SUCCESS",
+};
