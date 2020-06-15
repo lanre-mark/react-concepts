db69f28bf267f908167272c5eaad5c7fa8affab4
olanrewaju Makinde <lanre.developer@gmail.com>
2020-06-15 12:36:00 +0100

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./auth0/Header";
-import { LoginAuthProvider } from "./auth0/now/AuthContextProvider";
-import auth0Customize from "./auth0/now/AuthCustomize";
-import { GeneralDataContextProvider } from "./axios/Providers/GeneralDataProvider";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './auth0/Header';
+import { LoginAuthProvider } from './auth0/now/AuthContextProvider';
+import auth0Customize from './auth0/now/AuthCustomize';
+import { GeneralDataContextProvider } from './axios/Providers/GeneralDataProvider';
 
 /**
  *
@@ -12,18 +12,18 @@ import { GeneralDataContextProvider } from "./axios/Providers/GeneralDataProvide
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Demystifying React Concepts</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Demystifying React Concepts</h1>
       </header>
       <LoginAuthProvider
         config={{
-          clientID: "s5BA3M7qgE6YHeZgkzChE3Z5Lwv18rqF",
-          domain: "dev-mypandem.auth0.com",
+          clientID: 's5BA3M7qgE6YHeZgkzChE3Z5Lwv18rqF',
+          domain: 'dev-mypandem.auth0.com',
           options: auth0Customize,
         }}
-        memoryKey={"auth0:mee"}
+        memoryKey={'auth0:mee'}
       >
         <GeneralDataContextProvider>
           <Header />
