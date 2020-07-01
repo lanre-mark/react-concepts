245e20f09332dd38ba5a3439bf71a0e1baa30764
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-01 04:13:04 +0100

    google map example with Matrix/Route API

--- src/maptest/map.js
+++ src/maptest/map.js
@@ -1,10 +1,74 @@
-import React, { useRef, useState } from 'react';
-import { LoadScript, GoogleMap, Polygon } from '@react-google-maps/api';
+import React, { useRef, useState, useEffect, useMemo } from 'react';
+import {
+  LoadScript,
+  GoogleMap,
+  DirectionsRenderer,
+  DirectionsService,
+} from '@react-google-maps/api';
 
-​
 const mapContainerStyle = {
   height: '70vh',
   width: '100%',
+};
+
+const DirectionsAsAService = ({ destination, origin, callback }) => {
+  return useMemo(
+    <DirectionsService
+      options={{
+        destination: destination,
+        origin: origin,
+        travelMode: 'DRIVING',
+      }}
+      callback={callback}
+    />
+  );
+};
+
+// const DirectionsServiceMemo = React.useMemo(DirectionsAsAService);
+
+const Directions = (props) => {
+  const [directions, setDirections] = useState();
+  const { origin, destination } = props;
+  const waypoints = useRef({ origin, destination });
+  const count = useRef(0);
+
+  const options = {
+    polylineOptions: {
+      strokeColor: '#40c0a3',
+      strokeWeight: 6,
+      strokeOpacity: 0.8,
+    },
+  };
+
+  // useEffect(() => {
+  //   count.current = 0;
+  // }, [origin, destination]);
+
+  const directionsCallback = React.useCallback(function (result, status) {
+    if (status === 'OK' && count.current === 0) {
+      count.current++;
+      setDirections(result);
+    }
+  }, []);
+
+  return (
+    <>
+      <DirectionsService
+        options={{
+          destination,
+          origin,
+          travelMode: 'DRIVING',
+        }}
+        callback={directionsCallback}
+      />
+      {
+        directions &&
+          // <DirectionsRenderer directions={directions} options={options} />
+          console.log(directions)
+        //
+      }
+    </>
+  );
 };
 
 const Map04 = () => {
@@ -12,7 +76,13 @@ const Map04 = () => {
     lat: 52.52047739093263,
     lng: 13.36653284549709,
   });
+
+  const origin = { lng: 13.36653284549709, lat: 52.52047739093263 };
+  const destination = { lat: 53.3242381, lng: -6.3857848 };
+
+  const [directions, setDirections] = useState();
   const mapRef = useRef(null);
+  const count = useRef(0);
   const [zoom, setZoom] = useState(4);
   const handleMapLoaded = (map) => {
     mapRef.current = map;
@@ -26,14 +96,22 @@ const Map04 = () => {
   const onChangeZoom = () => {
     if (!mapRef.current) return;
     const currentZoom = mapRef.current.getZoom();
-    setZoom(currentZoom)
+    setZoom(currentZoom);
     //console.log(currentZoom);
   };
+
+  const directionsCallback = React.useCallback(function (result, status) {
+    if (status === 'OK' && count.current === 0) {
+      count.current++;
+      setDirections(result);
+    }
+  }, []);
+
   return (
     <div>
       <LoadScript
-         id="script-loader"
-        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        id='script-loader'
+        googleMapsApiKey=''
         language='en'
         region='us'
       >
@@ -44,8 +122,22 @@ const Map04 = () => {
           mapContainerStyle={mapContainerStyle}
           center={position}
           zoom={4}
-  
-        ></GoogleMap>
+        >
+          <Directions origin={origin} destination={destination} />
+
+          {/* <DirectionsAsAService
+            destination={destination}
+            origin={origin}
+            callback={directionsCallback}
+          /> */}
+
+          {/* {
+            directions &&
+              // <DirectionsRenderer directions={directions} options={options} />
+              console.log(directions)
+            //
+          } */}
+        </GoogleMap>
       </LoadScript>
     </div>
   );
