113e97dafdf797756a222bdda3b4d3a8a35fca12
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-14 16:53:13 +0100

    removed all test scenarios, integrated components testing to exten components

--- src/auth0/Header.js
+++ src/auth0/Header.js
@@ -8,6 +8,56 @@ import Map04 from '../maptest/map';
 import useClockTicker from '../exten/clock/clockhook';
 
 import Accordion from '../exten/collapsible/wrap-collapse';
+import WeatherIcon from '../weather-icons-lib';
+
+import { WeatherCard } from '../exten/weather-card';
+
+const weather1 = {
+  base: 'stations',
+  clouds: { all: 40 },
+  cod: 200,
+  coord: { lon: 3.39, lat: 6.45 },
+  dt: 1593826154,
+  id: 2332459,
+  main: {
+    feels_like: 31.35,
+    humidity: 94,
+    pressure: 1013,
+    temp: 26,
+    temp_max: 26,
+    temp_min: 26,
+  },
+  name: 'Lagos',
+  country: 'Nigeria',
+  sys: {
+    type: 1,
+    id: 1185,
+    country: 'NG',
+    sunrise: 1593840971,
+    sunset: 1593885917,
+  },
+  timezone: 3600,
+  visibility: 10000,
+  weather: [
+    {
+      id: 200,
+      main: 'Thunderstorm',
+      description: 'thunderstorm with light rain',
+      icon: '11n',
+    },
+  ],
+  wind: { speed: 1.5, deg: 250 },
+};
+
+const climaweather = {
+  feels_like: { value: 18.63, units: 'C' },
+  lat: 34.0531,
+  lon: -118.242,
+  observation_time: { value: '2020-07-09T11:58:27.070Z' },
+  precipitation: { value: 0, units: 'mm/hr' },
+  temp: { value: 18.63, units: 'C' },
+  weather_code: { value: 'Clear' },
+};
 
 const Header = () => {
   const {
@@ -43,6 +93,7 @@ const Header = () => {
   //   clockSize: '230px',
   //   date: clockDate,
   // };
+  // console.log(clockDate);
   // const clockPayload1 = {
   //   theme: {
   //     colorLightest: '#f0f0f0',
@@ -112,12 +163,38 @@ const Header = () => {
       <Clock {...clockPayload1}></Clock>
       <Clock {...clockPayload2}></Clock> */}
       {/* <Base></Base> */}
-      <div style={{ width: '20vw' }}>
+      {/* <div style={{ width: '20vw' }}>
         {Array(3)
           .fill('')
           .map((i, ii) => (
             <Accordion key={ii}></Accordion>
           ))}
+      </div> */}
+
+      {/* <WeatherIcon
+        timing={{ sunrise: 1594093958, sunset: 1594153075 }}
+        iconId='200'
+        className='icon'
+        rotate='0'
+        // fixedWidth={true}
+      /> */}
+
+      {/* <div style={{ width: '150%' }}>
+        <WeatherIcon
+          timing={{ sunrise: 1594093958, sunset: 1594153075 }}
+          iconId='905'
+          className='icon'
+          rotate='0'
+          fixedWidth={false}
+        />
+      </div> */}
+
+      {/* <WeatherCard
+        realtimeResponse={weather1}
+        location={'Los Angeles, California, United States'}
+      ></WeatherCard> */}
+      <div style={{ width: '25%' }}>
+        <Accordion></Accordion>
       </div>
     </div>
   );
